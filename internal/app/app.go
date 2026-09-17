c079591399b94cf7df9762550d5d9de9aeb8d377
Nevin Manoj <nevinmanojp@gmail.com>
2026-09-18 00:42:13 +0530

    updated CORS for new UI URL

--- internal/app/app.go
+++ internal/app/app.go
@@ -93,7 +93,7 @@ func Start() error {
 			if origin == "http://localhost:8081" {
 				return true
 			}
-			if origin == "https://jolly-smoke-0077dfc00.7.azurestaticapps.net" {
+			if origin == "gray-stone-0cf1a6200.2.azurestaticapps.net" {
 				return true
 			}
 			return false
